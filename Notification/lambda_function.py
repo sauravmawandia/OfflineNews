d5d29bb1f131dac308cae952f48a0fbab0aa6858
Abhinish Popli <popli.abhi@gmail.com>
2017-12-15 15:24:28 -0500

    updated lambda

--- Notification/lambda_function.py
+++ Notification/lambda_function.py
@@ -3,11 +3,11 @@ from __future__ import print_function
 import boto3
 
 sns = boto3.client('sns',
-                   aws_access_key_id="AKIAJDGSQ5JR3AOBERJA",
-                   aws_secret_access_key="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",
-                   region_name="us-east-1")
+                   aws_access_key_id="*** AWS Access Key ***",
+                   aws_secret_access_key="*** AWS Secret Key ***",
+                   region_name="*** AWS Region ***")
                    
-arn = 'arn:aws:sns:us-east-1:908644437130:app/APNS_SANDBOX/iNews'
+arn = '*** Your Application ARN ***'
 
 def lambda_handler(event, context):
     
